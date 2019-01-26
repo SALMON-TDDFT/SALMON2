41d32227e5576e4ccfc7b9af8c6a730e37846c04
mn2007 <noda@ccs.tsukuba.ac.jp>
2019-01-26 13:44:54 +0900

    use calc_myob_ud in src/GCEED/rt

--- src/GCEED/rt/projection.f90
+++ src/GCEED/rt/projection.f90
@@ -48,7 +48,7 @@ coef_mat=0.d0
 
 do iik=k_sta,k_end
 do iob=1,itotMST0
-  call calc_myob(iob,iob_myob)
+  call calc_myob_ud(iob,iob_myob,ilsda,nproc_ob,iparaway_ob,itotmst,nproc_ob_spin,mst)
   call check_corrkob(iob,iik,icorr_p,ilsda,nproc_ob,iparaway_ob,itotmst,k_sta,k_end,nproc_ob_spin,mst)
   if(icorr_p==1)then
 !$OMP parallel do private(iz,iy,ix)
