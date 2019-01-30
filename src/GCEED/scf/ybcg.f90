97db91005a632eaf64ae1e4c98615bf24dd4c77f
mn2007 <noda@ccs.tsukuba.ac.jp>
2019-01-30 16:25:55 +0900

    replace mg_sta etc with structure mg in ybcg

--- src/GCEED/scf/ybcg.f90
+++ src/GCEED/scf/ybcg.f90
@@ -29,8 +29,8 @@ use hpsi2_sub
 implicit none
 
 type(s_rgrid),intent(in) :: mg
-real(8):: psi_in(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3),  &
-               1:iobnum,k_sta:k_end)
+real(8):: psi_in(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3),  &
+                 1:iobnum,k_sta:k_end)
 integer :: iter,iob,job,iflag
 integer :: ix,iy,iz
 integer :: is,iobsta(2),iobend(2)
@@ -39,7 +39,9 @@ real(8) :: uk,alpha,Ak,Bk,Ck
 real(8) , allocatable :: xk(:,:,:),hxk(:,:,:),gk(:,:,:),pk(:,:,:)
 real(8) , allocatable :: gk2(:,:,:)
 real(8) :: elp2(2000)
-real(8):: tpsi(mg_sta(1)-Nd:mg_end(1)+Nd,mg_sta(2)-Nd:mg_end(2)+Nd,mg_sta(3)-Nd:mg_end(3)+Nd)
+real(8):: tpsi(mg%is_array(1):mg%ie_array(1),  &
+               mg%is_array(2):mg%ie_array(2),  &
+               mg%is_array(3):mg%ie_array(3))
 integer :: iob_myob,job_myob
 integer :: icorr,jcorr               
 integer :: iroot
@@ -48,11 +50,11 @@ character(30) :: commname
 
 commname='nproc_group_korbital'
 
-allocate (xk(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3)))
-allocate (hxk(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3)))
-allocate (gk(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3)))
-allocate (gk2(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3)))
-allocate (pk(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3)))
+allocate (xk(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3)))
+allocate (hxk(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3)))
+allocate (gk(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3)))
+allocate (gk2(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3)))
+allocate (pk(mg%is(1):mg%ie(1),mg%is(2):mg%ie(2),mg%is(3):mg%ie(3)))
 
 iwk_size=2
 call make_iwksta_iwkend
@@ -60,9 +62,9 @@ call make_iwksta_iwkend
 call set_isstaend(is_sta,is_end,ilsda,nproc_ob,nproc_ob_spin)
 
 !$OMP parallel do private(iz,iy,ix) 
-do iz=mg_sta(3)-Nd,mg_end(3)+Nd
-do iy=mg_sta(2)-Nd,mg_end(2)+Nd
-do ix=mg_sta(1)-Nd,mg_end(1)+Nd
+do iz=mg%is_array(3),mg%ie_array(3)
+do iy=mg%is_array(2),mg%ie_array(2)
+do ix=mg%is_array(1),mg%ie_array(1)
   tpsi(ix,iy,iz)=0.d0
 end do
 end do
@@ -92,18 +94,18 @@ orbital : do iob=iobsta(is),iobend(is)
   if(icorr==1)then
 
 !$OMP parallel do private(iz,iy,ix) 
-    do iz=mg_sta(3),mg_end(3)
-    do iy=mg_sta(2),mg_end(2)
-    do ix=mg_sta(1),mg_end(1)
+    do iz=mg%is(3),mg%ie(3)
+    do iy=mg%is(2),mg%ie(2)
+    do ix=mg%is(1),mg%ie(1)
       xk(ix,iy,iz)=psi_in(ix,iy,iz,iob_myob,1)
     end do
     end do
     end do
 
 !$OMP parallel do private(iz,iy,ix) 
-    do iz=mg_sta(3),mg_end(3)
-    do iy=mg_sta(2),mg_end(2)
-    do ix=mg_sta(1),mg_end(1)
+    do iz=mg%is(3),mg%ie(3)
+    do iy=mg%is(2),mg%ie(2)
+    do ix=mg%is(1),mg%ie(1)
       tpsi(ix,iy,iz)=xk(ix,iy,iz)
     end do
     end do
@@ -121,9 +123,9 @@ orbital : do iob=iobsta(is),iobend(is)
 
     if(icorr==1)then
 !$OMP parallel do private(iz,iy,ix) 
-      do iz=mg_sta(3),mg_end(3)
-      do iy=mg_sta(2),mg_end(2)
-      do ix=mg_sta(1),mg_end(1)
+      do iz=mg%is(3),mg%ie(3)
+      do iy=mg%is(2),mg%ie(2)
+      do ix=mg%is(1),mg%ie(1)
         gk(ix,iy,iz) = 2*( hxk(ix,iy,iz) - Rk*xk(ix,iy,iz) )
       end do
       end do
@@ -140,9 +142,9 @@ orbital : do iob=iobsta(is),iobend(is)
         call inner_product(mg,psi_in(:,:,:,job_myob,1),gk(:,:,:),sum0,commname)
         sum0=sum0*Hvol
 !$OMP parallel do private(iz,iy,ix)
-        do iz=mg_sta(3),mg_end(3)
-        do iy=mg_sta(2),mg_end(2)
-        do ix=mg_sta(1),mg_end(1)
+        do iz=mg%is(3),mg%ie(3)
+        do iy=mg%is(2),mg%ie(2)
+        do ix=mg%is(1),mg%ie(1)
           gk(ix,iy,iz)=gk(ix,iy,iz)-sum0*psi_in(ix,iy,iz,job_myob,1)
         end do
         end do
@@ -160,9 +162,9 @@ orbital : do iob=iobsta(is),iobend(is)
 
       if ( iter==1 ) then
 !$OMP parallel do private(iz,iy,ix)
-        do iz=mg_sta(3),mg_end(3)
-        do iy=mg_sta(2),mg_end(2)
-        do ix=mg_sta(1),mg_end(1)
+        do iz=mg%is(3),mg%ie(3)
+        do iy=mg%is(2),mg%ie(2)
+        do ix=mg%is(1),mg%ie(1)
           pk(ix,iy,iz) = -gk(ix,iy,iz)
         end do
         end do
@@ -170,9 +172,9 @@ orbital : do iob=iobsta(is),iobend(is)
       else
         uk=sum0/gkgk 
 !$OMP parallel do private(iz,iy,ix)
-        do iz=mg_sta(3),mg_end(3)
-        do iy=mg_sta(2),mg_end(2)
-        do ix=mg_sta(1),mg_end(1)
+        do iz=mg%is(3),mg%ie(3)
+        do iy=mg%is(2),mg%ie(2)
+        do ix=mg%is(1),mg%ie(1)
           pk(ix,iy,iz) = -gk(ix,iy,iz) + uk*pk(ix,iy,iz)
         end do
         end do
@@ -192,9 +194,9 @@ orbital : do iob=iobsta(is),iobend(is)
       pkHxk = pkHxk*Hvol
 
 !$OMP parallel do private(iz,iy,ix)
-      do iz=mg_sta(3),mg_end(3)
-      do iy=mg_sta(2),mg_end(2)
-      do ix=mg_sta(1),mg_end(1)
+      do iz=mg%is(3),mg%ie(3)
+      do iy=mg%is(2),mg%ie(2)
+      do ix=mg%is(1),mg%ie(1)
         tpsi(ix,iy,iz) = pk(ix,iy,iz)
       end do
       end do
@@ -227,9 +229,9 @@ orbital : do iob=iobsta(is),iobend(is)
   if(icorr==1)then
     call inner_product(mg,xk,xk,sum0,commname)
 !$OMP parallel do private(iz,iy,ix)
-    do iz=mg_sta(3),mg_end(3)
-    do iy=mg_sta(2),mg_end(2)
-    do ix=mg_sta(1),mg_end(1)
+    do iz=mg%is(3),mg%ie(3)
+    do iy=mg%is(2),mg%ie(2)
+    do ix=mg%is(1),mg%ie(1)
       psi_in(ix,iy,iz,iob_myob,1)=xk(ix,iy,iz)/sqrt(sum0*Hvol)
     end do
     end do
