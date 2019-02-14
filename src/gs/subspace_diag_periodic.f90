25310e9fdd8d1980f8e7bf1f11b32f07974c2846
mn2007 <noda@ccs.tsukuba.ac.jp>
2019-02-14 14:12:47 +0900

    modularize subspace_diag_periodic

--- src/gs/subspace_diag_periodic.f90
+++ src/gs/subspace_diag_periodic.f90
@@ -28,6 +28,7 @@ subroutine subspace_diag_periodic(mg,spsi,elp3,ilsda,nproc_ob,iparaway_ob,  &
   use salmon_communication, only: comm_bcast, comm_summation
   use misc_routines, only: get_wtime
   use hpsi_sub
+  use eigen_subdiag_periodic_sub
   implicit none
   type(s_rgrid),intent(in) :: mg
   type(s_wavefunction),intent(inout) :: spsi
