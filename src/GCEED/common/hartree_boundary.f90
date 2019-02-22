fd24f94ba83911c958ecf3e8a114b262018500b1
mn2007 <noda@ccs.tsukuba.ac.jp>
2019-02-22 14:24:28 +0900

    remove sendrecvh_sub from hartree_boundary

--- src/GCEED/common/hartree_boundary.f90
+++ src/GCEED/common/hartree_boundary.f90
@@ -22,7 +22,6 @@ use salmon_communication, only: comm_summation
 use misc_routines, only: get_wtime
 use scf_data
 use new_world_sub
-use sendrecvh_sub
 use allocate_mat_sub
 use deallocate_mat_sub
 
