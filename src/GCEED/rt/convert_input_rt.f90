577e63bd89baece9f7b7efdfd321973b07fa3368
mn2007 <noda@ccs.tsukuba.ac.jp>
2019-03-07 01:19:02 +0900

    move set_numcpu_rt

--- src/GCEED/rt/convert_input_rt.f90
+++ src/GCEED/rt/convert_input_rt.f90
@@ -17,6 +17,7 @@ subroutine convert_input_rt(Ntime)
 use salmon_parallel, only: nproc_id_global, nproc_size_global
 use salmon_communication, only: comm_is_root, comm_bcast
 use inputoutput
+use set_numcpu, only: set_numcpu_rt
 use scf_data
 use new_world_sub
 implicit none
