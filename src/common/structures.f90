f74ac9ed110e392d9a898f921a6cead9b1a744cf
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-03-11 14:51:06 +0900

    move_defination_srg

--- src/common/structures.f90
+++ src/common/structures.f90
@@ -38,6 +38,25 @@ module structures
     integer ,allocatable :: idx(:),idy(:),idz(:)    ! idx(is_overlap(1):ie_overlap(1))=is_array(1)~ie_array(1), ...
   end type s_rgrid
 
+  type s_sendrecv_grid
+    ! Size of grid system
+    type(s_rgrid) :: rg
+    ! Number of orbitals (4-th dimension of grid)
+    integer :: nb
+    ! Communicator
+    integer :: icomm, myrank
+    ! Neightboring MPI id (1:x,2:y,3:z, 1:upside,2:downside):
+    integer :: neig(1:3, 1:2) 
+    ! Communication requests (1:x,2:y,3:z, 1:upside,2:downside, 1:send,2:recv):
+    integer :: ireq(1:3, 1:2, 1:2)
+    ! PComm cache (1:x,2:y,3:z, 1:upside,2:downside, 1:src/2:dst)
+    type(s_pcomm_cache) :: cache(1:3, 1:2, 1:2)
+    ! Range (dim=1:x,2:y,3:z, dir=1:upside,2:downside, 1:src/2:dst, axis=1...3)
+    integer :: is_block(1:3, 1:2, 1:2, 1:3)
+    integer :: ie_block(1:3, 1:2, 1:2, 1:3)
+    logical :: pcomm_initialized
+  end type s_sendrecv_grid
+
   type s_wf_info
     logical :: if_divide_rspace
     integer :: irank_r(6)
@@ -140,25 +159,6 @@ module structures
     real(8), allocatable :: dbuf(:, :, :, :)
     complex(8), allocatable :: zbuf(:, :, :, :)
   end type s_pcomm_cache
-
-  type s_sendrecv_grid
-    ! Size of grid system
-    type(s_rgrid) :: rg
-    ! Number of orbitals (4-th dimension of grid)
-    integer :: nb
-    ! Communicator
-    integer :: icomm, myrank
-    ! Neightboring MPI id (1:x,2:y,3:z, 1:upside,2:downside):
-    integer :: neig(1:3, 1:2) 
-    ! Communication requests (1:x,2:y,3:z, 1:upside,2:downside, 1:send,2:recv):
-    integer :: ireq(1:3, 1:2, 1:2)
-    ! PComm cache (1:x,2:y,3:z, 1:upside,2:downside, 1:src/2:dst)
-    type(s_pcomm_cache) :: cache(1:3, 1:2, 1:2)
-    ! Range (dim=1:x,2:y,3:z, dir=1:upside,2:downside, 1:src/2:dst, axis=1...3)
-    integer :: is_block(1:3, 1:2, 1:2, 1:3)
-    integer :: ie_block(1:3, 1:2, 1:2, 1:3)
-    logical :: pcomm_initialized
-  end type s_sendrecv_grid
 
   type s_fourier_grid
     integer :: icomm_fourier
