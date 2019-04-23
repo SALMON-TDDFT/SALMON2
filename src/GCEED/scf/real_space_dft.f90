ae2a5478ed528c2461066d1c2772f63d567120d7
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-04-23 17:05:22 +0900

    comment_out_old_gs

--- src/GCEED/scf/real_space_dft.f90
+++ src/GCEED/scf/real_space_dft.f90
@@ -782,7 +782,7 @@ DFT_Iteration : do iter=1,iDiter(img)
           do iz=mg%is(3),mg%ie(3)
           do iy=mg%is(2),mg%ie(2)
           do ix=mg%is(1),mg%ie(1)
-            psi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%rwf(ix,iy,iz,is,iob,ik,1)
+            !psi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%rwf(ix,iy,iz,is,iob,ik,1)
           end do
           end do
           end do
@@ -797,7 +797,7 @@ DFT_Iteration : do iter=1,iDiter(img)
           do iz=mg%is(3),mg%ie(3)
           do iy=mg%is(2),mg%ie(2)
           do ix=mg%is(1),mg%ie(1)
-            zpsi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%zwf(ix,iy,iz,is,iob,ik,1)
+            !zpsi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%zwf(ix,iy,iz,is,iob,ik,1)
           end do
           end do
           end do
@@ -811,12 +811,12 @@ DFT_Iteration : do iter=1,iDiter(img)
     call timer_begin(LOG_CALC_GRAM_SCHMIDT)
     call gram_schmidt(system, mg, info, spsi) !uemoto!
     call debug_var_dump(system, mg, info, spsi, iter)  !uemoto!
-    select case(iperiodic)
-    case(0)
-      call Gram_Schmidt_ns
-    case(3)
-      call Gram_Schmidt_periodic
-    end select
+    !select case(iperiodic)
+    !case(0)
+    !  call Gram_Schmidt_ns
+    !case(3)
+    !  call Gram_Schmidt_periodic
+    !end select
   
     if(iflag_subspace_diag==1)then
       if(Miter>iDiter_nosubspace_diag)then
@@ -829,7 +829,7 @@ DFT_Iteration : do iter=1,iDiter(img)
               do iz=mg%is(3),mg%ie(3)
               do iy=mg%is(2),mg%ie(2)
               do ix=mg%is(1),mg%ie(1)
-                spsi%rwf(ix,iy,iz,is,iob,ik,1)=psi(ix,iy,iz,iob+(is-1)*info%numo,ik)
+                !spsi%rwf(ix,iy,iz,is,iob,ik,1)=psi(ix,iy,iz,iob+(is-1)*info%numo,ik)
               end do
               end do
               end do
