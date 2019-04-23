7f5325cfe4f8b75f953bfa5479aaca6606fd3aad
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-04-23 18:13:54 +0900

    fix

--- src/GCEED/scf/real_space_dft.f90
+++ src/GCEED/scf/real_space_dft.f90
@@ -777,26 +777,52 @@ DFT_Iteration : do iter=1,iDiter(img)
 
     call timer_begin(LOG_CALC_GRAM_SCHMIDT)
     call gram_schmidt(system, mg, info, spsi)
+
+    select case(iperiodic)
+
+
+    case(0)
+    do ik=k_sta,k_end
+      do iob=1,info%numo
+        do is=1,nspin
+          !$OMP parallel do private(iz,iy,ix)
+          do iz=mg%is(3),mg%ie(3)
+          do iy=mg%is(2),mg%ie(2)
+          do ix=mg%is(1),mg%ie(1)
+            psi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%rwf(ix,iy,iz,is,iob,ik,1)
+          end do
+          end do
+          end do
+        end do
+      end do
+      end do
+
+
+    case(3)
+
+    do ik=k_sta,k_end
+      do iob=1,info%numo
+        do is=1,nspin
+          !$OMP parallel do private(iz,iy,ix)
+          do iz=mg%is(3),mg%ie(3)
+          do iy=mg%is(2),mg%ie(2)
+          do ix=mg%is(1),mg%ie(1)
+            zpsi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%zwf(ix,iy,iz,is,iob,ik,1)
+          end do
+          end do
+          end do
+        end do
+      end do
+    end do
+    end select
+
+
+
     !call debug_var_dump(system, mg, info, spsi, iter)  !uemoto!
     if(iflag_subspace_diag==1)then
       if(Miter>iDiter_nosubspace_diag)then
         select case(iperiodic)
-        case(0)
-        do ik=k_sta,k_end
-          do iob=1,info%numo
-            do is=1,nspin
-              !$OMP parallel do private(iz,iy,ix)
-              do iz=mg%is(3),mg%ie(3)
-              do iy=mg%is(2),mg%ie(2)
-              do ix=mg%is(1),mg%ie(1)
-                psi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%rwf(ix,iy,iz,is,iob,ik,1)
-              end do
-              end do
-              end do
-            end do
-          end do
-          end do
-      
+        case(0)      
           call subspace_diag(mg,info,stencil,srg_ob_1,spsi,ilsda,nproc_ob,iparaway_ob,iobnum,itotmst,k_sta,k_end,mst,ifmst,hvol,  &
                 info_ob,bnmat,cnmat,hgs,ppg,vlocal)
 
@@ -816,21 +842,6 @@ DFT_Iteration : do iter=1,iDiter(img)
           end do
 
         case(3)
-
-        do ik=k_sta,k_end
-          do iob=1,info%numo
-            do is=1,nspin
-              !$OMP parallel do private(iz,iy,ix)
-              do iz=mg%is(3),mg%ie(3)
-              do iy=mg%is(2),mg%ie(2)
-              do ix=mg%is(1),mg%ie(1)
-                zpsi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%zwf(ix,iy,iz,is,iob,ik,1)
-              end do
-              end do
-              end do
-            end do
-          end do
-          end do
 
           call subspace_diag_periodic(mg,info,stencil,srg_ob_1,spsi,ilsda,nproc_ob,iparaway_ob,  &
                                       iobnum,itotmst,k_sta,k_end,mst,ifmst,hvol,   &
