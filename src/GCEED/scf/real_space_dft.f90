750058fba7b5344de99a956b3e3f2d6df96b58e0
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-04-24 10:50:22 +0900

    modified_gs_in_main_routine

--- src/GCEED/scf/real_space_dft.f90
+++ src/GCEED/scf/real_space_dft.f90
@@ -1260,7 +1260,7 @@ DFT_Iteration : do iter=1,iDiter(img)
     call timer_begin(LOG_CALC_GRAM_SCHMIDT)
     call gram_schmidt(system, mg, info, spsi)    
     call timer_end(LOG_CALC_GRAM_SCHMIDT)
-    
+
     select case(iperiodic)
     case(0)
       do ik=k_sta,k_end
@@ -1331,48 +1331,13 @@ DFT_Iteration : do iter=1,iDiter(img)
     call timer_end(LOG_CALC_MINIMIZATION)
 
 
-    select case(iperiodic)
-    case(0)
-      do ik=k_sta,k_end
-      do iob=1,info%numo
-        do is=1,nspin
-!$OMP parallel do private(iz,iy,ix)
-          do iz=mg%is(3),mg%ie(3)
-          do iy=mg%is(2),mg%ie(2)
-          do ix=mg%is(1),mg%ie(1)
-            psi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%rwf(ix,iy,iz,is,iob,ik,1)
-          end do
-          end do
-          end do
-        end do
-      end do
-      end do
-    case(3)
-      do ik=k_sta,k_end
-      do iob=1,info%numo
-        do is=1,nspin
-!$OMP parallel do private(iz,iy,ix)
-          do iz=mg%is(3),mg%ie(3)
-          do iy=mg%is(2),mg%ie(2)
-          do ix=mg%is(1),mg%ie(1)
-            zpsi(ix,iy,iz,iob+(is-1)*info%numo,ik)=spsi%zwf(ix,iy,iz,is,iob,ik,1)
-          end do
-          end do
-          end do
-        end do
-      end do
-      end do
-    end select
-
-
     call timer_begin(LOG_CALC_GRAM_SCHMIDT)
-    select case(iperiodic)
-    case(0)
-      call Gram_Schmidt_ns
-    case(3)
-      call Gram_Schmidt_periodic
-    end select
+    call gram_schmidt(system, mg, info, spsi)    
     call timer_end(LOG_CALC_GRAM_SCHMIDT)
+
+
+
+
 
 
     call timer_begin(LOG_CALC_RHO)
