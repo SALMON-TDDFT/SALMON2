834b3c1a5b92cf48af291c8e090b986f7c3968d0
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-07-25 16:09:18 +0900

    fix:  phase factor of Bloch orbitals

--- src/gs/band.f90
+++ src/gs/band.f90
@@ -20,19 +20,21 @@ module band
   contains
 
     !> Calculate inner-product to orbits in neightboring k-grid.
-    subroutine calc_kgrid_prod(system, rgrid_lg, rgrid_mg, wf_info, wavefunction, nk1, nk2, nk3, ndk, ik3d_tbl, prod_dk)
-        use structures
+    subroutine calc_kgrid_prod(sys, lg, mg, wfi, wf, nk1, nk2, nk3, ndk, ik3d_tbl, prod_dk)
+        use structures, only: s_dft_system, s_rgrid, s_orbital_parallel, s_orbital
+        use salmon_parallel, only: nproc_group_global, nproc_id_global
+        use math_constants, only: zI
         implicit none
-        type(s_dft_system), intent(in) :: system
-        type(s_rgrid), intent(in) :: rgrid_lg, rgrid_mg
-        type(s_orbital_parallel), intent(in) :: wf_info
-        type(s_orbital), intent(in) :: wavefunction
+        type(s_dft_system), intent(in) :: sys
+        type(s_rgrid), intent(in) :: lg, mg
+        type(s_orbital_parallel), intent(in) :: wfi
+        type(s_orbital), intent(in) :: wf
         integer, intent(in) :: nk1, nk2, nk3, ndk
         integer, intent(out) :: ik3d_tbl(1:3, nk1*nk2*nk3)
         complex(8), intent(out) :: prod_dk( &
             & nk1*nk2*nk3, &
             & -ndk:ndk, -ndk:ndk, -ndk:ndk, &
-            & system%no, system%no)
+            & sys%no, sys%no)
         
         integer, parameter :: nrep = 1
 
@@ -40,29 +42,32 @@ module band
         integer :: ik1, ik2, ik3
         integer :: jdk1, jdk2, jdk3
         complex(8) :: zwf_all( &
-            & rgrid_lg%is(1):rgrid_lg%ie(1), &
-            & rgrid_lg%is(2):rgrid_lg%ie(2), &
-            & rgrid_lg%is(3):rgrid_lg%ie(3), &
-            & system%nspin, system%no, system%nk)
+            & lg%is(1):lg%ie(1), &
+            & lg%is(2):lg%ie(2), &
+            & lg%is(3):lg%ie(3), &
+            & sys%nspin, sys%no, sys%nk)
         integer :: ik_tbl( &
-            & -nrep*nk1:(nrep+1)*nk1, &
-            & -nrep*nk2:(nrep+1)*nk2, &
-            & -nrep*nk3:(nrep+1)*nk3)
+            & (1-nrep*nk1):(nrep+1)*nk1, &
+            & (1-nrep*nk2):(nrep+1)*nk2, &
+            & (1-nrep*nk3):(nrep+1)*nk3)
 
         ! Create ik_tbl with periodic boundary condition:
         call create_ik_tbl()
 
-        ! Retrieve entire wavefunction:
+        ! Retrieve entire wf:
         call retrieve_entire_zwf()
 
         ! Calculate production <k,io|k+dk,jo> for all k,io,jo:
+        !$omp parallel do collapse(6) default(none) &
+        !$omp private(ik1, ik2, ik3, jdk1, jdk2, jdk3, ik, jk) &
+        !$omp shared(nk1, nk2, nk3, ndk, ik_tbl, prod_dk)
         do ik3 = 1, nk3
             do ik2 = 1, nk2
                 do ik1 = 1, nk1
-                    ik = ik_tbl(ik1, ik2, ik3)
                     do jdk3 = -ndk, ndk
                         do jdk2 = -ndk, ndk
                             do jdk1 = -ndk, ndk
+                                ik = ik_tbl(ik1, ik2, ik3)
                                 jk = ik_tbl(ik1+jdk1, ik2+jdk2, ik3+jdk3)
                                 call calc_prod(ik, jk, prod_dk(ik, jdk1, jdk2, jdk3, :, :))
                             end do
@@ -71,6 +76,7 @@ module band
                 end do
             end do
         end do
+        !$omp end parallel do
 
         return
 
@@ -115,40 +121,39 @@ module band
     subroutine retrieve_entire_zwf()
         use pack_unpack, only: copy_data
         use salmon_communication, only: comm_summation, comm_is_root
-        use salmon_parallel, only: nproc_group_global, nproc_id_global
       
         implicit none
         integer :: io
         integer, parameter :: im = 1, ispin = 1
         complex(8) :: zwf_all_tmp( &
-            & rgrid_lg%is(1):rgrid_lg%ie(1), &
-            & rgrid_lg%is(2):rgrid_lg%ie(2), &
-            & rgrid_lg%is(3):rgrid_lg%ie(3), &
-            & system%nspin, system%no, system%nk)
+            & lg%is(1):lg%ie(1), &
+            & lg%is(2):lg%ie(2), &
+            & lg%is(3):lg%ie(3), &
+            & sys%nspin, sys%no, sys%nk)
 
         zwf_all_tmp = 0d0
         
         call copy_data( &
-            wavefunction%zwf( &
-                & rgrid_mg%is(1):rgrid_mg%ie(1), &
-                & rgrid_mg%is(2):rgrid_mg%ie(2), &
-                & rgrid_mg%is(3):rgrid_mg%ie(3), &
-                & 1:system%nspin, &
-                & wf_info%io_s:wf_info%io_e, &
-                & wf_info%ik_s:wf_info%ik_e, &
-                & wf_info%im_s), &
+            wf%zwf( &
+                & mg%is(1):mg%ie(1), &
+                & mg%is(2):mg%ie(2), &
+                & mg%is(3):mg%ie(3), &
+                & 1:sys%nspin, &
+                & wfi%io_s:wfi%io_e, &
+                & wfi%ik_s:wfi%ik_e, &
+                & wfi%im_s), &
             zwf_all_tmp( &
-                & rgrid_mg%is(1):rgrid_mg%ie(1), &
-                & rgrid_mg%is(2):rgrid_mg%ie(2), &
-                & rgrid_mg%is(3):rgrid_mg%ie(3), &
-                & 1:system%nspin, &
-                & wf_info%io_s:wf_info%io_e, &
-                & wf_info%ik_s:wf_info%ik_e))
+                & mg%is(1):mg%ie(1), &
+                & mg%is(2):mg%ie(2), &
+                & mg%is(3):mg%ie(3), &
+                & 1:sys%nspin, &
+                & wfi%io_s:wfi%io_e, &
+                & wfi%ik_s:wfi%ik_e))
         
         call comm_summation( &
             & zwf_all_tmp, zwf_all, &
-            & system%ngrid*system%nspin*system%no*system%nk, &
-            & wf_info%icomm_rko)
+            & sys%ngrid*sys%nspin*sys%no*sys%nk, &
+            & wfi%icomm_rko)
 
         return
     end subroutine retrieve_entire_zwf
@@ -157,18 +162,43 @@ module band
     subroutine calc_prod(iik, jjk, prod_ij)
         implicit none
         integer, intent(in) :: iik, jjk
-        complex(8), intent(out) :: prod_ij(system%no, system%no)
+        complex(8), intent(out) :: prod_ij(sys%no, sys%no)
+
+        integer :: ir1, ir2, ir3
         integer :: iio, jjo
+        real(8) :: dk_ij(3), r(3), r_red(3)
+        complex(8) :: phase_ij( &
+            & mg%is(1):mg%ie(1), &
+            & mg%is(2):mg%ie(2), &
+            & mg%is(3):mg%ie(3), &
+            & sys%nspin)
 
         complex(8) ZDOTC ! From BLAS
 
-        do jjo = 1, system%no
-            do iio = 1, system%no
-                ! Compute dot-products: <iik,iio|jjk,jjo>
-                prod_ij(iio, jjo) = system%Hvol * ZDOTC( &
-                    & system%ngrid * system%nspin, &
+        ! Bloch wavenumber difference: dk_ij = ki - kj:
+        dk_ij(1:3) = sys%vec_k(1:3, iik) - sys%vec_k(1:3, jjk)
+        
+        do ir3 = mg%is(3), mg%ie(3)
+            do ir2 = mg%is(2), mg%ie(2)
+                do ir1 = mg%is(1), mg%ie(1)
+                    ! ii-th grid-point in cartesian coordinate: r
+                    r(1:3) = sys%primitive_a(1:3, 1) * sys%hgs(1) * ir1 &
+                        &  + sys%primitive_a(1:3, 2) * sys%hgs(2) * ir2 &
+                        &  + sys%primitive_a(1:3, 3) * sys%hgs(3) * ir3 
+                    ! phase factor: exp(-i(ki-kj)r)
+                    phase_ij(ir1, ir2, ir3, :) = exp(-zI * dot_product(dk_ij, r))
+                end do
+            end do
+        end do
+
+        do jjo = 1, sys%no
+            do iio = 1, sys%no
+                ! Compute dot-product of two eigenstate psi(ik,io) and psi(jk,jo):
+                ! <psi(ik,io)|psi(jk,jo)> = <u(ik,io)|exp(-i(ki-kj))*u(jk,j)>
+                prod_ij(iio, jjo) = sys%Hvol * ZDOTC( &
+                    & sys%ngrid * sys%nspin, &
                     & zwf_all(:, :, :, :, iio, iik), 1, &
-                    & zwf_all(:, :, :, :, jjo, jjk), 1)
+                    & zwf_all(:, :, :, :, jjo, jjk) * phase_ij(:, :, :, :), 1)
             end do
         end do
 
