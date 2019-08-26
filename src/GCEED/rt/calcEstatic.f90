f2f7235267ad748422c5875712c0bd2547d1747f
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-08-27 01:49:36 +0900

    refactor: reduce iwksta/iwkend

--- src/GCEED/rt/calcEstatic.f90
+++ src/GCEED/rt/calcEstatic.f90
@@ -33,17 +33,6 @@ real(8) :: Ex_static2(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3
 real(8) :: Ey_static2(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3))
 real(8) :: Ez_static2(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3))
 
-iwksta(1:3)=ista_Mxin_s(1:3,nproc_id_global)
-iwkend(1:3)=iend_Mxin_s(1:3,nproc_id_global)
-iwk2sta(1:3)=ista_Mxin_s(1:3,nproc_id_global)-Ndh
-iwk2end(1:3)=iend_Mxin_s(1:3,nproc_id_global)+Ndh
-iwk3sta(1:3)=ista_Mxin_s(1:3,nproc_id_global)
-iwk3end(1:3)=iend_Mxin_s(1:3,nproc_id_global)
-iwknum(1:3)=iwkend(1:3)-iwksta(1:3)+1
-iwk2num(1:3)=iwk2end(1:3)-iwk2sta(1:3)+1
-iwk3num(1:3)=iwk3end(1:3)-iwk3sta(1:3)+1
-
-
 !$OMP parallel do private(iz,iy,ix)
 do iz=ng%is(3)-Ndh,ng%ie(3)+Ndh
 do iy=ng%is(2)-Ndh,ng%ie(2)+Ndh
