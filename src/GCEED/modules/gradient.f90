01338c0797271ce171553aedf7c77a10d007d169
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-08-27 10:50:11 +0900

    refactor: remove of gradient2

--- src/GCEED/modules/gradient.f90
+++ src/GCEED/modules/gradient.f90
@@ -16,7 +16,6 @@
 MODULE gradient_sub
 
 use scf_data
-use gradient2_sub
 use structures, only: s_rgrid, s_sendrecv_grid
 use sendrecv_grid, only: update_overlap_complex8, update_overlap_real8
 use pack_unpack, only: copy_data
@@ -73,7 +72,45 @@ call copy_data( &
     & is_array_wk(3):ie_array_wk(3)))
   
 call update_overlap_real8(srg, mg, tmp)
-call calc_gradient2(tmp, grad_tmp)
+
+if(Nd<=3)then
+!$OMP parallel do private(iz,iy,ix) 
+  do iz=mg%is(3),mg%ie(3)
+  do iy=mg%is(2),mg%ie(2)
+  do ix=mg%is(1),mg%ie(1)
+    grad_tmp(1,ix,iy,iz)=      &
+      (tmp(ix+1,iy,iz)-tmp(ix-1,iy,iz))/2.d0/Hgs(1)
+    grad_tmp(2,ix,iy,iz)=      &
+      (tmp(ix,iy+1,iz)-tmp(ix,iy-1,iz))/2.d0/Hgs(2)
+    grad_tmp(3,ix,iy,iz)=      &
+      (tmp(ix,iy,iz+1)-tmp(ix,iy,iz-1))/2.d0/Hgs(3)
+  end do
+  end do
+  end do
+else if(Nd==4)then
+!$OMP parallel do private(iz,iy,ix)
+  do iz=mg%is(3),mg%ie(3)
+  do iy=mg%is(2),mg%ie(2)
+  do ix=mg%is(1),mg%ie(1)
+    grad_tmp(1,ix,iy,iz)=      &
+      (bN1*(tmp(ix+1,iy,iz)-tmp(ix-1,iy,iz))      &
+      +bN2*(tmp(ix+2,iy,iz)-tmp(ix-2,iy,iz))      &
+      +bN3*(tmp(ix+3,iy,iz)-tmp(ix-3,iy,iz))      &
+      +bN4*(tmp(ix+4,iy,iz)-tmp(ix-4,iy,iz)))/Hgs(1) 
+    grad_tmp(2,ix,iy,iz)=      &
+      (bN1*(tmp(ix,iy+1,iz)-tmp(ix,iy-1,iz))      &
+      +bN2*(tmp(ix,iy+2,iz)-tmp(ix,iy-2,iz))      &
+      +bN3*(tmp(ix,iy+3,iz)-tmp(ix,iy-3,iz))      &
+      +bN4*(tmp(ix,iy+4,iz)-tmp(ix,iy-4,iz)))/Hgs(2) 
+    grad_tmp(3,ix,iy,iz)=      &
+      (bN1*(tmp(ix,iy,iz+1)-tmp(ix,iy,iz-1))      &
+      +bN2*(tmp(ix,iy,iz+2)-tmp(ix,iy,iz-2))      &
+      +bN3*(tmp(ix,iy,iz+3)-tmp(ix,iy,iz-3))      &
+      +bN4*(tmp(ix,iy,iz+4)-tmp(ix,iy,iz-4)))/Hgs(3) 
+  end do
+  end do
+  end do
+end if
 
 call copy_data( &
   & grad_tmp(1:3, &
@@ -115,6 +152,7 @@ complex(8) :: grad_tmp(1:3, &
   & mg%is_array(1):mg%ie_array(1), &
   & mg%is_array(2):mg%ie_array(2), &
   & mg%is_array(3):mg%ie_array(3))
+integer :: ix, iy, iz
 
   
   call copy_data( &
@@ -128,7 +166,45 @@ complex(8) :: grad_tmp(1:3, &
     & is_array_wk(3):ie_array_wk(3)))
   
 call update_overlap_complex8(srg, mg, tmp)
-call calc_gradient2(tmp, grad_tmp)
+
+if(Nd<=3)then
+!$OMP parallel do private(iz,iy,ix) 
+  do iz=mg%is(3),mg%ie(3)
+  do iy=mg%is(2),mg%ie(2)
+  do ix=mg%is(1),mg%ie(1)
+    grad_tmp(1,ix,iy,iz)=      &
+      (tmp(ix+1,iy,iz)-tmp(ix-1,iy,iz))/2.d0/Hgs(1)  
+    grad_tmp(2,ix,iy,iz)=      &
+      (tmp(ix,iy+1,iz)-tmp(ix,iy-1,iz))/2.d0/Hgs(2)  
+    grad_tmp(3,ix,iy,iz)=      &
+      (tmp(ix,iy,iz+1)-tmp(ix,iy,iz-1))/2.d0/Hgs(3)  
+  end do
+  end do
+  end do
+else if(Nd==4)then
+!$OMP parallel do private(iz,iy,ix) 
+  do iz=mg%is(3),mg%ie(3)
+  do iy=mg%is(2),mg%ie(2)
+  do ix=mg%is(1),mg%ie(1)
+    grad_tmp(1,ix,iy,iz)=      &
+      (bN1*(tmp(ix+1,iy,iz)-tmp(ix-1,iy,iz))      &
+      +bN2*(tmp(ix+2,iy,iz)-tmp(ix-2,iy,iz))      &
+      +bN3*(tmp(ix+3,iy,iz)-tmp(ix-3,iy,iz))      &
+      +bN4*(tmp(ix+4,iy,iz)-tmp(ix-4,iy,iz)))/Hgs(1) 
+    grad_tmp(2,ix,iy,iz)=      &
+      (bN1*(tmp(ix,iy+1,iz)-tmp(ix,iy-1,iz))      &
+      +bN2*(tmp(ix,iy+2,iz)-tmp(ix,iy-2,iz))      &
+      +bN3*(tmp(ix,iy+3,iz)-tmp(ix,iy-3,iz))      &
+      +bN4*(tmp(ix,iy+4,iz)-tmp(ix,iy-4,iz)))/Hgs(2) 
+    grad_tmp(3,ix,iy,iz)=      &
+      (bN1*(tmp(ix,iy,iz+1)-tmp(ix,iy,iz-1))      &
+      +bN2*(tmp(ix,iy,iz+2)-tmp(ix,iy,iz-2))      &
+      +bN3*(tmp(ix,iy,iz+3)-tmp(ix,iy,iz-3))      &
+      +bN4*(tmp(ix,iy,iz+4)-tmp(ix,iy,iz-4)))/Hgs(3) 
+  end do
+  end do
+  end do
+end if
 
 call copy_data( &
   & grad_tmp(1:3, &
