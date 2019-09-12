ad91442455b2b4da69622c86ee7bd79829368e65
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2019-09-12 15:40:56 +0900

    filesystem: short fix.

--- cmakefiles/check_compiler_features.cmake
+++ cmakefiles/check_compiler_features.cmake
@@ -21,18 +21,19 @@ end]]
 FORTRAN_COMPILER_HAS_2MB_ALIGNED_ALLOCATION SRC_EXT F90)
 
 
+set(CMAKE_REQUIRED_DEFINITIONS "-D_XOPEN_SOURCE=500")
 check_include_file(unistd.h SYSTEM_HAS_POSIX)
 if (SYSTEM_HAS_POSIX)
-  set(CMAKE_REQUIRED_DEFINITIONS "-D_XOPEN_SOURCE=500")
   check_symbol_exists(stat   "sys/stat.h;sys/types.h;unistd.h" SYSTEM_HAS_POSIX_STAT)
   check_symbol_exists(access "unistd.h"                        SYSTEM_HAS_POSIX_ACCESS)
   check_symbol_exists(mkdir  "sys/stat.h;sys/types.h;unistd.h" SYSTEM_HAS_POSIX_MKDIR)
-  check_symbol_exists(remove "stdio.h"                         SYSTEM_HAS_STDIO_REMOVE)
-  check_symbol_exists(nftw    "ftw.h"                          SYSTEM_HAS_POSIX_NFTW)
-  unset(CMAKE_REQUIRED_DEFINITIONS)
+  check_symbol_exists(nftw   "ftw.h"                           SYSTEM_HAS_POSIX_NFTW)
 else ()
   message(FATAL_ERROR "SALMON requires POSIX API for IO access")
 endif ()
+unset(CMAKE_REQUIRED_DEFINITIONS)
+
+check_symbol_exists(remove "stdio.h" SYSTEM_HAS_STDIO_REMOVE)
 
 check_symbol_exists(PATH_MAX "limits.h"       SYSTEM_HAS_PATH_MAX_IN_LIMITS_H)
 check_symbol_exists(PATH_MAX "linux/limits.h" SYSTEM_HAS_PATH_MAX_IN_LINUX_LIMITS_H)
