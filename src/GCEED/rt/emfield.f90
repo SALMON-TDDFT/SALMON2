f0011c5ebab91762074cc3159ed524157631ec69
syamada0 <syamada@ccs.tsukuba.ac.jp>
2019-09-24 10:21:35 +0900

    revert calc_Aext

--- src/GCEED/rt/emfield.f90
+++ src/GCEED/rt/emfield.f90
@@ -42,15 +42,45 @@ end subroutine calc_emfields
 subroutine calcAext
 !$ use omp_lib
 use em_field, only: calc_Ac_ext
-use scf_data, only: dt,Miter_rt,itotNtime,A_ext
+use scf_data!, only: dt,Miter_rt,itotNtime,A_ext
 implicit none
-integer :: itt
-real(8) :: tt
+integer :: j2
+integer :: ipulse
+real(8) :: env_trigon_1
+real(8) :: env_trigon_2
 
-do itt=Miter_rt+1,itotNtime+1
-  tt = dt*dble(itt)
-  call calc_Ac_ext(tt,A_ext(:,itt))
-end do
+select case(ikind_eext)
+  case(0)
+    A_ext(1:3,:)=0.d0
+    A_ext(1,:)=Fst*epdir_re1(1)
+    A_ext(2,:)=Fst*epdir_re1(2)
+    A_ext(3,:)=Fst*epdir_re1(3)
+  case(1,15)
+    A_ext(1:3,:)=0.d0
+    do itt=Miter_rt+1,itotNtime+1
+      if(dt*dble(itt) <= tw1)then
+        ipulse=1
+        call calc_env_trigon(ipulse,env_trigon_1)
+        do j2=1,3
+          A_ext(j2,itt)=A_ext(j2,itt)+E_amplitude1*epdir_re1(j2)*env_trigon_1
+        end do
+      end if
+      if(dt*dble(itt)-t1_t2 >= 1.d-12 .and. dt*dble(itt)-t1_t2 <= tw2)then
+        ipulse=2
+        call calc_env_trigon(ipulse,env_trigon_2)
+        do j2=1,3
+          A_ext(j2,itt)=A_ext(j2,itt)+E_amplitude2*epdir_re2(j2)*env_trigon_2
+        enddo
+      end if
+    end do
+end select
+
+!integer :: itt
+!real(8) :: tt
+!do itt=Miter_rt+1,itotNtime+1
+!  tt = dt*dble(itt)
+!  call calc_Ac_ext(tt,A_ext(:,itt))
+!end do
 
 return
 end subroutine calcAext
