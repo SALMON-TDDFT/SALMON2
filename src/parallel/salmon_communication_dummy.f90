9f6b14b0906406f840f026756a4bb1eb93e2d2fe
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2019-10-14 18:23:15 +0900

    Fix MPI dummy module.

--- src/parallel/salmon_communication_dummy.f90
+++ src/parallel/salmon_communication_dummy.f90
@@ -1080,7 +1080,7 @@ contains
     integer, intent(in)  :: ngroup
     UNUSED_VARIABLE(ngroup)
     !NOP! ABORT_MESSAGE(ngroup,"comm_allgatherv_array1d_double")
-    outvalue(displs(1)+1:displs(1)+ncounts(1)-1) = invalue(1:ncounts(1)-1)
+    outvalue(displs(1)+1:displs(1)+1+ncounts(1)-1) = invalue(1:ncounts(1)-1)
   end subroutine
 
 
