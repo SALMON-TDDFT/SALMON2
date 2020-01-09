953d3a82eec710c2985ca2cc154a961a868be5fb
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2020-01-09 13:24:09 +0900

    fix Libxc download URI.

--- cmakefiles/build_required_packages.cmake
+++ cmakefiles/build_required_packages.cmake
@@ -53,7 +53,7 @@ if (USE_LIBXC)
     message(STATUS "Build Libxc version ${LIBXC_VERSION}")
 
     ExternalProject_Add(libxc-project
-      URL              "http://www.tddft.org/programs/octopus/down.php?file=libxc/${LIBXC_VERSION}/libxc-${LIBXC_VERSION}.tar.gz"
+      URL              "http://www.tddft.org/programs/libxc/down.php?file=${LIBXC_VERSION}/libxc-${LIBXC_VERSION}.tar.gz"
       PREFIX           "${CMAKE_BINARY_DIR}/libxc"
       CMAKE_ARGS       -D BUILD_SHARED_LIBS=off -D ENABLE_FORTRAN=on -D ENABLE_XHOST=off
                        -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -D CMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}
