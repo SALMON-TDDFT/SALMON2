0a03570d7bc50fd3c309ce471ff5be5cbfed6a15
syamada0 <syamada@ccs.tsukuba.ac.jp>
2020-02-27 20:42:09 +0900

    scalapack

--- src/gs/subspace_diagonalization.f90
+++ src/gs/subspace_diagonalization.f90
@@ -502,9 +502,9 @@ do ispin = 1, system%nspin
 
 !  call eigen_subdiag_periodic(zhmat, zevec, system%no, ierr)
   if(yn_pdsyev=='y') then
-     stop "open the subroutine eigen_real8_pdsyev in code"
+!     stop "open the subroutine eigen_real8_pdsyev in code"
 ! OPEN here to use scalapack pdsyev
-!     call eigen_real8_pdsyev(pinfo, info, hmat, eval, evec)
+     call eigen_real8_pdsyev(pinfo, info, hmat, eval, evec)
   else
      call eigen_real8_dsyev(hmat, eval, evec)
   endif
