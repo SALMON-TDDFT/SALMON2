4872e42763c15d9fd42e6b3166332ddd08a39861
h76007 <h76007@ofp06.ofp>
2020-02-28 01:10:53 +0900

    update

--- src/gs/subspace_diagonalization.f90
+++ src/gs/subspace_diagonalization.f90
@@ -291,6 +291,7 @@ end subroutine
         & wf2_block(:, :, :, jo))
     end do
 
+    wf_block_send = 0d0
     hmat_tmp = 0d0
     do m = 0, pinfo%nporbital - 1
 
@@ -349,6 +350,9 @@ end subroutine
     !$omp workshare
     wf2_block = 0d0
     !$omp end workshare
+
+    wf_block_send = 0d0
+
     do m = 0, pinfo%nporbital - 1
 
       if(m == info%id_o ) then
