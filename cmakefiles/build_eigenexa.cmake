d9e1ba69d9d7a6edacb11b2c58bb20f0309c2b1e
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2020-03-19 10:46:35 +0900

    bugfix: cross compiling mode problem of EigenExa.

--- cmakefiles/build_eigenexa.cmake
+++ cmakefiles/build_eigenexa.cmake
@@ -8,11 +8,27 @@ if (USE_EIGENEXA)
   set(EIGENEXA_VERSION "2.4b")
   message(STATUS "Build RIKEN R-CCS EigenExa library version ${EIGENEXA_VERSION}")
 
+
+  # set dummy option to avoid autotools checking...
+  set(HOST_OPTION)
+  if (${CMAKE_CROSSCOMPILING})
+    set(HOST_OPTION "--host=x86_64")
+  endif ()
+
+  # compiler option, but skip fujitsu environment.
+  if (IS_FUJITSU_COMPILER)
+  else ()
+    set(CC     "CC=${CMAKE_C_COMPILER}")
+    set(FC     "FC=${CMAKE_Fortran_COMPILER}")
+    set(CFLAGS "CFLAGS=${CMAKE_C_FLAGS}")
+    set(FFLAGS "FFLAGS=${CMAKE_Fortran_FLAGS}")
+  endif()
+
   ExternalProject_Add(eigenexa-project
     URL               "https://www.r-ccs.riken.jp/labs/lpnctrt/assets/img/EigenExa-${EIGENEXA_VERSION}.tgz"
     SOURCE_DIR        ${CMAKE_CURRENT_BINARY_DIR}/eigenexa
     BINARY_DIR        ${CMAKE_CURRENT_BINARY_DIR}/eigenexa
-    CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/eigenexa/configure --prefix=${CMAKE_CURRENT_BINARY_DIR} CC=${CMAKE_C_COMPILER} F77=${CMAKE_Fortran_COMPILER} "CFLAGS=${CMAKE_C_FLAGS}" "FFLAGS=${CMAKE_Fortran_FLAGS}"
+    CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/eigenexa/configure ${HOST_OPTION} --prefix=${CMAKE_CURRENT_BINARY_DIR} ${CC} ${FC} ${CFLAGS} ${FFLAGS}
     BUILD_COMMAND     make
     STEP_TARGETS      install
     EXCLUDE_FROM_ALL  on
