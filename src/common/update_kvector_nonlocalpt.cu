2b17a0aaf7f76bc789e16d8017bb5db6f8d03d4b
Hiroyuki Ootomo <hiroyuki.otomo@fixstars.com>
2021-05-06 11:12:23 +0900

    Update `update_kvector_nonlocalpt_kernel` to use `cuDoubleComplex` instead of `double complex`

--- src/common/update_kvector_nonlocalpt.cu
+++ src/common/update_kvector_nonlocalpt.cu
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <complex.h>
+#include <complex>
 #include <math.h>
+#include <cuComplex.h>
 
+extern "C" {
 void update_kvector_nonlocalpt_kernel(
 		// Output
-		double complex* ppg_zekr_uV,
+		cuDoubleComplex* ppg_zekr_uV,
 		// Input (size)
 		const int ik_s,
 		const int ik_e,
@@ -27,14 +29,15 @@ void update_kvector_nonlocalpt_kernel(
 				const double y = ppg_rxyz[1 + j * (3) + iatom * (3 * ppg_nps)];
 				const double z = ppg_rxyz[2 + j * (3) + iatom * (3 * ppg_nps)];
 
-				const double complex ekr = cexp(
-						I * (
-							kAc[0 + 3 * (ik - (ik_s - 1))] * x +
-							kAc[1 + 3 * (ik - (ik_s - 1))] * y +
-							kAc[2 + 3 * (ik - (ik_s - 1))] * z)
-						);
-				ppg_zekr_uV[j + ilma * (ppg_nps) + ik * (ppg_nps * ppg_nlma)] = conj(ekr) * ppg_uv[j + ilma * (ppg_nps)];
+				const double theta = kAc[0 + 3 * (ik - (ik_s - 1))] * x +
+					kAc[1 + 3 * (ik - (ik_s - 1))] * y +
+					kAc[2 + 3 * (ik - (ik_s - 1))] * z;
+				const cuDoubleComplex ekr_conj = {cos(theta), -sin(theta)};
+				const double tmp = ppg_uv[j + ilma * (ppg_nps)];
+				const cuDoubleComplex res = {ekr_conj.x * tmp, ekr_conj.y * tmp};
+				ppg_zekr_uV[j + ilma * (ppg_nps) + ik * (ppg_nps * ppg_nlma)] = res;
 			}
 		}
 	}
+}
 }
