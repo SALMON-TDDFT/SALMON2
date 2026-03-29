1919abbc392be3aec2b754c60457fdf8030633c3
otobetomohito <otobe.tomohito@qst.go.jp>
2026-03-29 16:40:24 +0900

    fix: restore dg hartree main route for local rho

--- src/poisson/poisson_dg_distributed.f90
+++ src/poisson/poisson_dg_distributed.f90
@@ -630,6 +630,84 @@ contains
     deallocate(rho_full%f)
   end subroutine hartree_dg_via_global_fullrho
 
+  subroutine hartree_dg_via_parent_localrho(info, lg, mg, fg, poisson, dg_frag, rho, Vh)
+    use structures,           only: s_rgrid, s_reciprocal_grid, s_poisson, s_scalar, s_parallel_info
+    use rt_dg_fragment_types, only: s_dg_fragment_rt
+    use poisson_periodic,     only: poisson_ft, poisson_ffte, poisson_ft_hse_sr, poisson_ffte_hse_sr
+    use inputoutput,          only: yn_ffte, yn_put_wall_z_boundary, wall_height, wall_width
+    use salmon_global,        only: hse_omega
+    use math_constants,       only: pi
+    implicit none
+
+    type(s_parallel_info),  intent(in)    :: info
+    type(s_rgrid),           intent(in)    :: lg, mg
+    type(s_reciprocal_grid), intent(in)    :: fg
+    type(s_poisson),         intent(inout) :: poisson
+    type(s_dg_fragment_rt),  intent(in)    :: dg_frag
+    type(s_scalar),          intent(in)    :: rho
+    type(s_scalar),          intent(inout) :: Vh
+
+    character(16) :: env_hse_sr
+    logical :: use_hse_sr_hartree
+    integer :: env_status
+    integer :: ix, iy, iz
+    real(8) :: z, z0, Vwall_z
+
+    env_hse_sr = ''
+    use_hse_sr_hartree = .false.
+    call get_environment_variable('SALMON_HSE_SR_HARTREE', env_hse_sr, status=env_status)
+    if (env_status == 0) then
+      select case(trim(adjustl(env_hse_sr)))
+      case('1','y','Y','yes','YES','true','TRUE','on','ON')
+        use_hse_sr_hartree = .true.
+      end select
+    end if
+    if (use_hse_sr_hartree .and. hse_omega <= 0.0d0) then
+      stop 'poisson_dg_distributed: hse_omega must be > 0 when SALMON_HSE_SR_HARTREE is enabled'
+    end if
+
+    if (dg_frag%id == 0) then
+      write(*,'(1x,a,4(a,i0))') "        hartree trace: using-parent-localrho-main", &
+        " id=", dg_frag%id, " isize=", dg_frag%isize, " id_frag=", dg_frag%id_frag, " isize_frag=", dg_frag%isize_frag
+      flush(6)
+    end if
+
+    if (yn_ffte == 'y') then
+      if (use_hse_sr_hartree) then
+        call poisson_ffte_hse_sr(lg, mg, info, fg, rho, Vh, poisson, hse_omega)
+      else
+        call poisson_ffte(lg, mg, info, fg, rho, Vh, poisson)
+      end if
+    else
+      if (use_hse_sr_hartree) then
+        call poisson_ft_hse_sr(lg, mg, info, fg, rho, Vh, poisson, hse_omega)
+      else
+        call poisson_ft(lg, mg, info, fg, rho, Vh, poisson)
+      end if
+    end if
+
+    if (yn_put_wall_z_boundary == 'y') then
+      !$omp parallel do private(iz,iy,ix,z,z0,Vwall_z) schedule(static)
+      do iz = mg%is(3), mg%ie(3)
+        z = iz * dg_frag%hgs(3)
+        z0 = lg%num(3) * dg_frag%hgs(3)
+        if (z <= wall_width) then
+          Vwall_z = wall_height * cos((z / wall_width) * pi / 2.0d0)**2
+        else if (z >= z0 - wall_width) then
+          Vwall_z = wall_height * cos(((z0 - z) / wall_width) * pi / 2.0d0)**2
+        else
+          cycle
+        end if
+        do iy = mg%is(2), mg%ie(2)
+          do ix = mg%is(1), mg%ie(1)
+            Vh%f(ix, iy, iz) = Vh%f(ix, iy, iz) + Vwall_z
+          end do
+        end do
+      end do
+      !$omp end parallel do
+    end if
+  end subroutine hartree_dg_via_parent_localrho
+
   ! hartree_dg_distributed
   !   Compute Vh%f = Hartree potential from rho%f using a distributed
   !   dimension-by-dimension DFT (same algorithm as poisson_ft, but
@@ -661,24 +739,18 @@ contains
       end select
     end if
 
-    ! The global distributed Hartree core requires full rho on dg_frag%icomm.
-    ! DG fragment RT currently provides rho on the parent local-grid partition,
-    ! so rebuild full rho globally before entering the core path.
-    use_fragment_local_poisson = use_fragment_local_poisson .or. any(mg%num(1:3) /= lg%num(1:3))
-    use_fragment_local_poisson = use_fragment_local_poisson .or. any(mg%is(1:3) /= lg%is(1:3))
-    use_fragment_local_poisson = use_fragment_local_poisson .or. any(mg%ie(1:3) /= lg%ie(1:3))
-    use_fragment_local_poisson = use_fragment_local_poisson .or. lbound(rho%f,1) /= mg%is(1)
-    use_fragment_local_poisson = use_fragment_local_poisson .or. ubound(rho%f,1) /= mg%ie(1)
-    use_fragment_local_poisson = use_fragment_local_poisson .or. lbound(rho%f,2) /= mg%is(2)
-    use_fragment_local_poisson = use_fragment_local_poisson .or. ubound(rho%f,2) /= mg%ie(2)
-    use_fragment_local_poisson = use_fragment_local_poisson .or. lbound(rho%f,3) /= mg%is(3)
-    use_fragment_local_poisson = use_fragment_local_poisson .or. ubound(rho%f,3) /= mg%ie(3)
     if (use_fragment_local_poisson) then
-      call hartree_dg_via_global_fullrho(info, lg, mg, fg, poisson, dg_frag, rho, Vh)
+      call hartree_dg_via_poisson_periodic(lg, mg, fg, poisson, dg_frag, rho, Vh)
+      return
+    end if
+    if (lbound(rho%f,1) == mg%is(1) .and. ubound(rho%f,1) == mg%ie(1) .and. &
+        lbound(rho%f,2) == mg%is(2) .and. ubound(rho%f,2) == mg%ie(2) .and. &
+        lbound(rho%f,3) == mg%is(3) .and. ubound(rho%f,3) == mg%ie(3)) then
+      call hartree_dg_via_parent_localrho(info, lg, mg, fg, poisson, dg_frag, rho, Vh)
       return
     end if
 
-    call hartree_dg_fullrho_core(lg, mg, fg, poisson, dg_frag, rho, Vh)
+    call hartree_dg_via_global_fullrho(info, lg, mg, fg, poisson, dg_frag, rho, Vh)
   end subroutine hartree_dg_distributed
 
   ! hartree_dg_fullrho_core
